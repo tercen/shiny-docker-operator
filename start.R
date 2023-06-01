e9ff2a066a156b633ddca51f89be9c09e907a8fe
Alex <alexandre.maurel@gmail.Com>
2023-06-01 16:30:04 +0200

    TERCEN_WEB_APP_PORT added

--- start.R
+++ start.R
@@ -1,3 +1,4 @@
 options("tercen.serviceUri"=Sys.getenv("TERCEN_SERVICE_URI"))
 options("shiny.host"="0.0.0.0")
-shiny::runApp(port=8080,launch.browser=FALSE)
+port=as.integer(Sys.getenv("TERCEN_WEB_APP_PORT", unset = "8080"))
+shiny::runApp(port=port,launch.browser=FALSE)
